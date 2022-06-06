5001e29b2a0bdcd5993628074f357bb804f28f03
runeblaze <runeblaze@excite.co.jp>
2022-06-06 14:06:42 -0400

    multi RF distance support

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use clap::{ArgEnum, Parser, Subcommand};
 use ogcat::TreeCollection;
 use serde::Serialize;
 use serde_json::json;
+use std::fmt::Display;
 use std::path::PathBuf;
 use tabled::{builder::Builder, Style};
 use tabled::{Table, Tabled};
@@ -26,6 +27,13 @@ enum Format {
     Json,
 }
 
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ArgEnum, Debug)]
+enum Approx {
+    Auto,
+    Yes,
+    No,
+}
+
 #[derive(Subcommand, Debug)]
 enum SubCommand {
     Rf {
@@ -35,6 +43,13 @@ enum SubCommand {
         estimated: PathBuf,
         #[clap(short, long)]
         fast: bool,
+    },
+
+    RfMulti {
+        #[clap(short, long = "ref")]
+        reference: PathBuf,
+        #[clap(short, long = "est")]
+        estimated: PathBuf,
     },
 
     RfAllpairs {
@@ -60,6 +75,30 @@ enum SubCommand {
     },
 }
 
+fn two_pass_mean_stddev(x: &[f64]) -> (f64, f64) {
+    let s1 = x.iter().sum::<f64>();
+    let mean = s1 / x.len() as f64;
+    let s2 = x.iter().map(|&x| (x - mean) * (x - mean)).sum::<f64>();
+    let sample_var = (s2 as f64 / (x.len() as f64 - 1f64)) as f64;
+    let sample_stddev = sample_var.sqrt();
+    (mean, sample_stddev)
+}
+
+#[derive(Debug, Serialize)]
+pub enum RFMultiType {
+    OneToMany,
+    ManyPairs,
+}
+
+impl Display for RFMultiType {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            RFMultiType::OneToMany => write!(f, "OneToMany"),
+            RFMultiType::ManyPairs => write!(f, "ManyPairs"),
+        }
+    }
+}
+
 #[derive(Debug, Serialize)]
 pub struct RFPairResult {
     pub pair: (usize, usize),
@@ -70,6 +109,45 @@ pub struct RFPairResult {
 pub struct RFPairResults {
     pub files: Option<Vec<String>>,
     pub results: Vec<RFPairResult>,
+}
+
+#[derive(Debug, Serialize)]
+pub struct RFMultiResult {
+    pub nrecords: usize,
+    pub multi_type: RFMultiType,
+    pub nrf_summary: (f64, f64),
+    pub rf_rate_summary: (f64, f64),
+    pub fp_rate_summary: (f64, f64),
+    pub fn_rate_summary: (f64, f64),
+    pub results: Vec<RFPrettyOutput>,
+}
+
+impl RFMultiResult {
+    pub fn new(multi_type: RFMultiType, results: Vec<RFPrettyOutput>) -> Self {
+        let mut nrfs: Vec<f64> = vec![];
+        let mut rfs: Vec<f64> = vec![];
+        let mut fps: Vec<f64> = vec![];
+        let mut fns: Vec<f64> = vec![];
+        for result in &results {
+            nrfs.push(result.n_rf);
+            rfs.push(result.rf_rate);
+            fps.push(result.fp_rate);
+            fns.push(result.fn_rate);
+        }
+        let nrf_summary = two_pass_mean_stddev(&nrfs);
+        let rf_rate_summary = two_pass_mean_stddev(&rfs);
+        let fp_rate_summary = two_pass_mean_stddev(&fps);
+        let fn_rate_summary = two_pass_mean_stddev(&fns);
+        RFMultiResult {
+            nrecords: results.len(),
+            multi_type,
+            nrf_summary,
+            rf_rate_summary,
+            fp_rate_summary,
+            fn_rate_summary,
+            results,
+        }
+    }
 }
 
 fn execute_rf_allpairs(trees: &[String], format: Format) {
@@ -147,6 +225,76 @@ fn execute_rf(reference: &PathBuf, estimated: &PathBuf, fast: bool, format: Form
     }
 }
 
+fn format_uncertainty_percentage(d: &(f64, f64)) -> String {
+    let (mean, stddev) = d;
+    format!("{:.2}% ± {:.2}%", mean * 100f64, stddev * 100f64)
+}
+
+fn execute_rf_multi(reference: &PathBuf, estimated: &PathBuf, format: Format) {
+    let mut trees = TreeCollection::from_newick(reference).unwrap();
+    let ntaxa = trees.ntaxa();
+    let ngenes = trees.ngenes();
+    trees.add_trees(estimated).unwrap();
+    assert_eq!(
+        ntaxa,
+        trees.ntaxa(),
+        "Number of taxa in reference and estimated trees do not match"
+    );
+
+    if ngenes > 1 {
+        assert_eq!(
+            ngenes * 2,
+            trees.ngenes(),
+            "Number of genes in reference and estimated trees do not match"
+        );
+    }
+
+    let rf_outputs = if ngenes > 1 {
+        ogcat::compare_many2many(&trees)
+    } else {
+        ogcat::compare_one2many(&trees)
+    };
+    let pretty = rf_outputs
+        .iter()
+        .map(|it| RFPrettyOutput::new(*it))
+        .collect::<Vec<_>>();
+    let multiresult = RFMultiResult::new(
+        if ngenes > 1 {
+            RFMultiType::ManyPairs
+        } else {
+            RFMultiType::OneToMany
+        },
+        pretty,
+    );
+    match format {
+        Format::Human => {
+            let table = Builder::default()
+                .set_columns([
+                    "nrecords",
+                    "multi_type",
+                    "n_rf",
+                    "rf_rate",
+                    "fp_rate",
+                    "fn_rate",
+                ])
+                .add_record([
+                    multiresult.nrecords.to_string(),
+                    multiresult.multi_type.to_string(),
+                    format_uncertainty_percentage(&multiresult.nrf_summary),
+                    format_uncertainty_percentage(&multiresult.rf_rate_summary),
+                    format_uncertainty_percentage(&multiresult.fp_rate_summary),
+                    format_uncertainty_percentage(&multiresult.fn_rate_summary),
+                ])
+                .build()
+                .with(Style::modern());
+            println!("{}", table.to_string());
+        }
+        Format::Json => {
+            println!("{}", serde_json::to_string_pretty(&multiresult).unwrap());
+        }
+    }
+}
+
 fn main() {
     let args = Args::parse();
     match args.cmd {
@@ -194,23 +342,22 @@ fn main() {
                         "{:.3}%",
                         (res.gap_cells as f64) / res.total_cells as f64 * 100.0
                     ),
-                    format!("{:.3}", res.avg_sequence_length),
-                    format!("{:.3}", p_result.as_ref().unwrap().avg_pdis),
-                    format!("{:.3}", p_result.unwrap().max_pdis),
-                ]).build();
+                    format!("{:.4}", res.avg_sequence_length),
+                    format!("{:.4}", p_result.as_ref().unwrap().avg_pdis),
+                    format!("{:.4}", p_result.unwrap().max_pdis),
+                ])
+                .build();
             println!("{}", table.with(Style::modern()).to_string());
-            // if pdis {
-
-            // } else {
-
-            // }
-            // println!("{}", Table::new([&res]).with(Style::modern()).to_string());
-            // let pdis = ;
-            // println!("{}", Table::new([&pdis]).with(Style::modern()).to_string());
         }
         SubCommand::RfAllpairs { trees } => {
             execute_rf_allpairs(&trees, args.format);
-        },
+        }
+        SubCommand::RfMulti {
+            reference,
+            estimated,
+        } => {
+            execute_rf_multi(&reference, &estimated, args.format);
+        }
         _ => {
             panic!("Unsupported subcommand");
         }
